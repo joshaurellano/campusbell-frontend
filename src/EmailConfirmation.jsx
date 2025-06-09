1a5cb73cff867490cbdd70d28af80b89aeea6d51
josh <jaaurellano@gbox.ncf.edu.ph>
2025-06-09 20:47:26 +0800

    Add box shadow to card holding the otp verification form;Added loading state for otp button

--- src/EmailConfirmation.jsx
+++ src/EmailConfirmation.jsx
@@ -21,18 +21,26 @@ function EmailConfirmation () {
     const [otp,setOtp] = useState('');
     const [error,setError] = useState('');
 
+    const[buttonLoading, setButtonLoading] = useState(false);
+
 const otpVerify = async (e) => {
+    setButtonLoading(true);
+    setError('');
     e.preventDefault();
     console.log(email, otp)
     try {
             const response = await axios.post(`${API_ENDPOINT}otp/verify`,{
             email,
             otp});
-        
+
+        setButtonLoading(false);
         console.log(response)
         setEmail('');
         setOtp('');
+
+        navigate('/login');
     } catch(error) {
+        setButtonLoading(false);
         setError(error.response.data.message)
     }
 }
@@ -57,7 +65,7 @@ const otpVerify = async (e) => {
             justifyContent:'center'
                 }}>
 
-           <Card style={{height:'250px', width:'400px'}}>
+           <Card style={{minHeight:'250px', width:'400px', boxShadow:'black 1px 1px', borderRadius:'15px', padding:'8px'}}>
             <Card.Body>
                 <div>
                     <span>Please check the email sent to you </span>
@@ -68,34 +76,53 @@ const otpVerify = async (e) => {
                     height:'100%',
                     alignItems:'center',
                 }}>
-                <Form style={{width:'100%', borderRadius:'15px'}} onSubmit={otpVerify}>
+                <Form style={{width:'100%', borderRadius:'15px', marginTop:'8px'}} onSubmit={otpVerify}>
                     <div style={{width:'100%', marginBottom:'8px'}}>
                     <Form.Group>
-                        <Form.Control placeholder='Enter Email' style={{borderRadius:'15px'}}
-                        value={email}
-                        onChange={(e)=>setEmail(e.target.value)}>
-                        </Form.Control>
+                        <FloatingLabel label='Email'>
+                            <Form.Control placeholder='Enter Email' style={{borderRadius:'15px'}}
+                            value={email}
+                            onChange={(e)=>setEmail(e.target.value)}>
+                            </Form.Control>
+                        </FloatingLabel>  
                     </Form.Group>
                     </div>
 
                     <div style={{width:'100%'}}>
                     <Form.Group>
-                        <Form.Control placeholder='Enter OTP' style={{borderRadius:'15px'}}
-                        value={otp}
-                        onChange={(e)=>setOtp(e.target.value)}>
-                        </Form.Control>
+                        <FloatingLabel label='OTP'>
+                            <Form.Control placeholder='Enter OTP' style={{borderRadius:'15px'}}
+                            value={otp}
+                            onChange={(e)=>setOtp(e.target.value)}>
+                            </Form.Control>
+                        </FloatingLabel>
                     </Form.Group>
                     </div>
 
                     <div style={{marginTop:'8px'}}>
                     <Form.Group>
-                        <Button style={{borderRadius:'20px', width:'120px'}} type='submit'>Confirm</Button>
+                        <Button disabled={buttonLoading} style={{borderRadius:'20px', width:'120px'}} type='submit'>
+                          {
+                            buttonLoading ? (
+                                <div>
+                                    <Spinner animation="border" size='sm' />
+                                </div>
+
+                            ) : (
+                                'Confirm'
+                            )
+                          }
+                        
+                            </Button>
                     </Form.Group>
                     </div>
                     <div style={{marginTop:'8px'}}>
                         {error && <span style={{color:'red'}}>{error}</span>}
                     </div>
                 </Form>
+                </div>
+                <div style={{fontSize:'12px'}}>
+                    <span>Didn't received the code?</span> <span>Request for new one</span>
                 </div>
             </Card.Body>
             </Card> 
