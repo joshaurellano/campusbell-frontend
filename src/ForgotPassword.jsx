1d6ef0206467d68cac2ce027a9144ff737e2c5d1
josh <jaaurellano@gbox.ncf.edu.ph>
2025-07-09 19:44:18 +0800

    Added toast to reset password page

--- src/ForgotPassword.jsx
+++ src/ForgotPassword.jsx
@@ -1,17 +1,63 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {useNavigate, useParams} from 'react-router-dom';
-
-import {Nav,Navbar,Container,Button,Form,Row,Col,Spinner,Card,FloatingLabel} from 'react-bootstrap';
+import {Nav,Navbar,Container,Button,Form,Row,Col,Spinner,Card,FloatingLabel,Toast,ToastContainer} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 
 import {API_ENDPOINT} from './Api';
+import axios from 'axios';
 
 function ForgotPassword() {
-const { token } = useParams();
-const navigate = useNavigate();
-console.log(token)
+    const { token } = useParams();
+    const navigate = useNavigate();
+
+    const [saveToken, setSaveToken] = useState('')
+    const [error, setError] = useState('')
+    const [errorToast, setErrorToast] = useState(false)
+
+    const openToast = () => setErrorToast(true)
+    const closeToast = () => {
+        setErrorToast(false)
+        navigate('/login')    
+    }
+
+    const verifyToken = async () => {
+        try {
+            const resetToken = await axios.get(`${API_ENDPOINT}auth/password-reset/${token}`)
+            setSaveToken(resetToken)
+        } catch (error) {
+            setError(error.response.data.message);
+            openToast();
+            // navigate('/login');
+        }
+    }
+
+    useEffect(() =>{
+        verifyToken();
+    },[])
   return (
     <div style={{ height:'100vh',width:'100vw',display:'flex', justifyContent:'center',alignItems:'center'}}>
+        {
+            errorToast && (
+                <>
+                    <ToastContainer
+                        position='top-end'>
+                    <Toast show={openToast} onClose={closeToast}
+                        bg='danger'>
+                        <Toast.Header closeButton={true}>
+                             <img
+                                src="holder.js/20x20?text=%20"
+                                className="rounded me-2"
+                                alt=""
+                                />
+                            <strong className="me-auto"> Error</strong>
+                             
+                        </Toast.Header>
+                        <Toast.Body className="text-white" style={{color:'white'}}> {error}</Toast.Body>
+                    </Toast>
+                    </ToastContainer>
+                </>
+            )
+        }
         <Card style={{width:'300px',height:'300px'}}>
             <Card.Body>
                 <div style={{
